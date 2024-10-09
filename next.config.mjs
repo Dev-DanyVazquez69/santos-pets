874598bb5b01e1b6830bf8930db183028dcca314
Dev-DanyVazquez69 <danyv7818@gmail.com>
2024-10-09 16:51:20 -0300

    implement PWA app

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,83 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,      // Habilita o modo estrito do React
+    swcMinify: true,            // Habilita minificação com SWC para melhor performance
+    compiler: {
+        removeConsole: process.env.NODE_ENV !== "development", // Remove console.logs no ambiente de produção
+    },
+    // Função para adicionar os headers de segurança
+    async headers() {
+        return [
+            {
+                // Aplica esses headers a todas as rotas
+                source: '/(.*)',
+                headers: securityHeaders,
+            },
+            {
+                source: '/sw.js',
+                headers: securityHeadersWork
+            }
+        ];
+    },
+};
+
+// Headers de segurança
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self'; font-src 'self';",
+  },
+  {
+    //Controla a quantidade de informações do referenciador incluídas nas solicitações, equilibrando segurança e funcionalidade.
+    key: 'Referrer-Policy',
+    value: 'no-referrer',
+  },
+  {
+    //Impede o sniffing do tipo MIME, reduzindo o risco de uploads de arquivos maliciosos.
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    //Protege contra ataques de clickjacking, impedindo que seu site seja incorporado em iframes.
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    //Garante que o site seja acessado apenas por HTTPS.
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload',
+  },
+  {
+    //Controla o uso de recursos sensíveis, como geolocalização, câmera e microfone.
+    key: 'Permissions-Policy',
+    value: 'geolocation=(), camera=(), microphone=()',
+  },
+];
+
+const securityHeadersWork = [
+  {
+    //Garante que o service worker seja interpretado corretamente como JavaScript.
+    key: 'Content-Type',
+    value: 'application/javascript; charset=utf-8',
+  },
+  {
+    // Impede o armazenamento em cache do service worker, garantindo que os usuários sempre obtenham a versão mais recente.
+    key: 'Cache-Control',
+    value: 'no-cache, no-store, must-revalidate',
+  },
+  {
+    //Implementa uma Política de Segurança de Conteúdo rigorosa para o service worker, permitindo apenas scripts da mesma origem.
+    key: 'Content-Security-Policy',
+    value: "default-src 'self'; script-src 'self'",
+  },
+]
+
+// Configuração PWA, separada da configuração dos headers
+export default withPWA({
+    dest: "public",                          // Destino dos arquivos do PWA
+    disable: process.env.NODE_ENV === "development", // Desabilita PWA no ambiente de desenvolvimento
+    register: true,                          // Registra o service worker
+    skipWaiting: true,                       // Ativa imediatamente o novo service worker
+})(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,15 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Santos Petz",
   description: "Santos Petz",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "next14", "pwa", "next-pwa"],
+  authors: [
+    {
+      name: "Daniel",
+      url: "https://www.linkedin.com/in/daniel-santos-pessoa/",
+    },
+  ],
 };
 
 export default function RootLayout({
@@ -25,6 +34,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="apple-mobile-web-app-title" content="Santos Petz" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
