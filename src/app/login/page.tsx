87642fca6730f0fe19236c0e8747be1a186b9037
Dev-DanyVazquez69 <danyv7818@gmail.com>
2024-10-21 16:57:22 -0300

    add type page login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 import { signIn } from "@/app/auth"
 import Image from "next/image"
 
-export const Login = () => {
+export const Login: React.FC = () => {
     return (
         <div className="w-screen h-screen flex flex-col items-center justify-center bg-blue">
             <div className="relative w-40 h-40 rounded-full overflow-hidden flex items-center justify-center">
@@ -17,9 +17,9 @@ export const Login = () => {
                     await signIn("google")
                 }}
             >
-                <button 
-                type="submit"
-                className="bg-background p-2 rounded ">
+                <button
+                    type="submit"
+                    className="bg-background p-2 rounded ">
                     Faça Login com o Google
                 </button>
             </form>
