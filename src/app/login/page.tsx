a43b63936d42a46f8ef10c437f676a5a595e5b18
Dev-DanyVazquez69 <danyv7818@gmail.com>
2024-10-21 16:55:31 -0300

    start implementing google authentication

---
+++ src/app/login/page.tsx
@@ -0,0 +1,30 @@
+import { signIn } from "@/app/auth"
+import Image from "next/image"
+
+export const Login = () => {
+    return (
+        <div className="w-screen h-screen flex flex-col items-center justify-center bg-blue">
+            <div className="relative w-40 h-40 rounded-full overflow-hidden flex items-center justify-center">
+                <Image
+                    src={'/web-app-manifest-512x512.png'}
+                    alt="logo"
+                    fill
+                    className="object-cover" />
+            </div>
+            <form
+                action={async () => {
+                    "use server"
+                    await signIn("google")
+                }}
+            >
+                <button 
+                type="submit"
+                className="bg-background p-2 rounded ">
+                    Faça Login com o Google
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
