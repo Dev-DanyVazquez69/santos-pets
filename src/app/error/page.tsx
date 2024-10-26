48b5bd7f701b4df24ea48bb2a9fa9d1703bdb22b
Dev-DanyVazquez69 <danyv7818@gmail.com>
2024-10-26 14:59:58 -0300

    test

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -4,19 +4,13 @@
 
 import React from "react";
 
-interface ErrorPageProps {
-  error: Error;
-  reset: () => void;
-}
-
-const ErrorPage: React.FC<ErrorPageProps> = ({ error, reset }) => {
-  return (
-    <div>
-      <h1>Ocorreu um erro</h1>
-      <p>{error.message}</p>
-      <button onClick={reset}>Tentar novamente</button>
-    </div>
-  );
+const ErrorPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Ocorreu um erro</h1>
+            <button>Tentar novamente</button>
+        </div>
+    );
 };
 
 export default ErrorPage;
