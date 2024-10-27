68d47a052e1f80f6d04a933489f02fc396cc1907
Dev-DanyVazquez69 <danyv7818@gmail.com>
2024-10-27 13:04:07 -0300

    ajust middlera

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,5 +10,5 @@ export default auth((req) => {
 })
 
 export const config = {
-    matcher: ['/((?!_next/static|_next/image|favicon.ico|api/public|signin|public|register).*)'],
+    matcher: ['/((?!public|_next/static|_next/image|signin|register).*)'],
 }
